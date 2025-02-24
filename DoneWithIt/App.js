a9c51c2553bbce8fb1d8aca6795b56daaf3b0f8e
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-02-24 03:46:29 +0300

    Update App.js
    get to know props of text API, and the safe area view

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+const handlePress = () =>
+{
+  console.log("CLicked");
+}
   return (
     <View style={styles.container}>
-      <Text>Bixx Tech.</Text>
+      <Text numberOfLines={1} onPress={handlePress}>this is a very long text guys i wish i hadnt done something like that in a minute you know tory lanes</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
