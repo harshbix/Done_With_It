77799b457cee10e2fe9597b96bd52af687f7c187
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2025-02-24 23:37:08 +0300

    Update App.js
    the alert methods and how to prompt them seamlessly

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Button, SafeAreaView, Alert } from 'react-native';
 
 export default function App() {
 
 
   return (
    <SafeAreaView style={styles.container}>
-    <Button title='Click Me' color={"black"} onPress={()=> alert('Button clicked!')}/>
+    <Button title='Click Me' color={"black"} onPress={()=> Alert.alert("The title", "Hey you modafaka", 
+    
+      [ { text : "Yes"},
+        {text : "No"}]
+    
+    )}/>
    </SafeAreaView>
   );
 }
